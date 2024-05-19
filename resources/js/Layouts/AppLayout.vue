d902b4a9b49d9f0544eee8168dbf171f219ae6f1
Nellie87 <nelliwndungu@gmail.com>
2024-05-19 15:49:57 +0300

    minor correction on nav part

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -102,7 +102,7 @@ onMounted(() => {
 
         <Banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100" style="padding-top: 4rem;">
             <nav class="bg-white border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
