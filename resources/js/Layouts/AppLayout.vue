e2886eef8b21529f5fa8f9ca872be2d6d9c5aff1
Nellie87 <nelliwndungu@gmail.com>
2024-06-27 13:46:36 +0300

    profile pic

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -238,8 +238,9 @@ const goBack = () => {
                                         <button v-if="$page.props.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
                                             <!-- <img class="h-8 w-8 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_path" :alt="$page.props.auth.user.name"> -->
                                             <!-- <img src="{{Storage::url($this->user->profile_photo_path)}}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover"> -->
-                                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Storage::url(Auth::user()->profile_photo_path) }}" />
-                                        </button>
+                                            <img class="h-8 w-8 rounded-full object-cover" src="/images/cover-04.jpg" />
+                                        </button>     
+
 
                                         <span v-else class="inline-flex rounded-md">
                                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
